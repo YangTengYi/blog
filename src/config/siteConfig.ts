68925970eda6865c9223da248692dcecabb7b08e
杨腾壹 <13595973+yang-tengyi@user.noreply.gitee.com>
2026-07-01 14:37:26 +0800

    1.1

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -6,13 +6,13 @@ const SITE_LANG = "zh_CN";
 
 export const siteConfig: SiteConfig = {
 	// 站点标题
-	title: "Firefly",
+	title: "虚无缥缈の小破站",
 
 	// 站点副标题
 	subtitle: "Demo site",
 
 	// 站点 URL
-	site_url: "https://firefly.cuteleaf.cn",
+	site_url: "https://xuwupiaomiao.cn",
 
 	// 站点描述
 	description:
