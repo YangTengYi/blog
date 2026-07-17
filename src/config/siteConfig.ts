155cfc7d72b3dce5a9be1a86e82ea6c6bce5d7bd
YangTengYi <YangTengYi@users.noreply.github.com>
2026-07-17 08:28:34 +0800

    配置2

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -86,7 +86,7 @@ export const siteConfig: SiteConfig = {
 		// 4. 网络图片: { type: "url", value: "https://example.com/logo.png", alt: "Logo" }
 		logo: {
 			type: "image",
-			value: "assets/images/logo.webp",
+			value: "assets/images/firefly.png",
 			alt: "🍀",
 		},
 		// 导航栏标题
@@ -102,7 +102,7 @@ export const siteConfig: SiteConfig = {
 	},
 
 	// 站点开始日期，用于统计运行天数
-	siteStartDate: "2026-04-27",
+	siteStartDate: "2026-05-20",
 
 	// 站点时区（IANA 时区字符串），用于格式化bangumi、rss里的构建日期时间等等..
 	// 示例："Asia/Shanghai", "UTC", 如果为空，则按照构建服务器的时区进行时区转换
