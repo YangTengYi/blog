587a0069a5c95f7a3bb84cd701effeeb56cec4b3
YangTengYi <YangTengYi@users.noreply.github.com>
2026-09-21 08:49:57 +0800

    update: 2026-09-21 08:49

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -279,7 +279,7 @@ export const siteConfig: SiteConfig = {
 	// 分页配置
 	pagination: {
 		// 每页显示的文章数量
-		postsPerPage: 10,
+		postsPerPage: 15,
 	},
 
 	// ── 文章内容页配置 ──────────────────────────────────
